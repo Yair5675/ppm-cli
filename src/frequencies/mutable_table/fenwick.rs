0f79695ff1adc497dfd3cf48be1446808451c20a
Yair5675 <yairziv5675@gmail.com>
2025-04-07 02:09:41 +0300

    Started the FenwickTree struct

--- src/frequencies/mutable_table/fenwick.rs
+++ src/frequencies/mutable_table/fenwick.rs
@@ -14,3 +14,30 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use crate::number_types::CalculationsType;
+
+/// Computes the least significant set bit of a number
+fn lsb(n: usize) -> usize {
+    let n = n as isize;
+    (n & -n) as usize
+}
+
+/// A data structure that allows efficient calculation of cumulative summation AND mutation of
+/// values
+pub struct FenwickTree {
+    // Values of the tree, allow for quick computation of cumulative sum AND mutation of values.
+    // It uses Box since we never append/remove elements, only mutate them:
+    data: Box<[CalculationsType]>,
+}
+
+impl FenwickTree {
+    /// Creates a new, empty FenwickTree with the given size
+    pub fn new(size: usize) -> Self {
+        // Fenwick trees index calculations depend on the indices starting at 1, so add an extra
+        // element to ensure this:
+        Self {
+            data: vec![0; size + 1].into_boxed_slice()
+        }
+    }
+}

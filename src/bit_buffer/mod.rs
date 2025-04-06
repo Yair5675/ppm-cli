533eff46735f6f271a06b24cb2ccdb89eff35d39
Yair5675 <yairziv5675@gmail.com>
2025-04-07 00:45:04 +0300

    Made the bit_iter module public for external use

--- src/bit_buffer/mod.rs
+++ src/bit_buffer/mod.rs
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-mod bit_iter;
+pub mod bit_iter;
 #[cfg(test)]
 mod unit_tests;
 

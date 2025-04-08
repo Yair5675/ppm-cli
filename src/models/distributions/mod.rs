051b1eebfa9b939181a95f96104740df08c8aee6
Yair5675 <yairziv5675@gmail.com>
2025-04-08 14:26:36 +0300

    Created the UniformDistributionModel struct, with an optional index assigned to an escape symbol

--- src/models/distributions/mod.rs
+++ src/models/distributions/mod.rs
@@ -14,3 +14,49 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use std::num::NonZero;
+use thiserror::Error;
+
+/// A probability model that assigns each index an equal probability
+pub struct UniformDistributionModel {
+    /// Number of symbols the model supports
+    num_symbols: NonZero<usize>,
+    /// The index assigned to the escape symbol
+    escape_idx: Option<usize>,
+}
+
+impl UniformDistributionModel {
+    /// Initializes a UniformDistributionModel without an escape index (see `new_with_escape` if you
+    /// want an escape index).
+    /// 
+    /// ## Parameters:
+    /// * num_symbols - Number of symbols in the model, cannot be zero.
+    pub fn new(num_symbols: NonZero<usize>) -> Self {
+        Self {
+            num_symbols, escape_idx: None
+        }
+    }
+    
+    /// Initializes a UniformDistributionModel with an assigned escape index.
+    /// 
+    /// ## Parameters:
+    /// * num_symbols - Number of symbols in the model **including the escape symbol**, cannot be 0.
+    /// * escape_idx - The index chosen for the escape symbol.
+    /// 
+    /// ## Possible Failures:
+    /// The function will return Err(EscapeIndexTooLarge) if escape_idx >= num_symbols
+    pub fn new_with_escape(num_symbols: NonZero<usize>, escape_idx: usize) -> Result<Self, EscapeIndexTooLarge> {
+        if escape_idx >= num_symbols.get() {
+            Err(EscapeIndexTooLarge(num_symbols.get(), escape_idx))
+        } else {
+            Ok(Self {
+                num_symbols, escape_idx: Some(escape_idx)
+            })
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+#[error("The number of symbols in the model is {0}, yet the index chosen for the escape symbol is {1}")]
+pub struct EscapeIndexTooLarge(usize, usize);

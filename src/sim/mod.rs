965727fd47f78b5855a2aff5c5e625d5636fa0e3
Yair5675 <yairziv5675@gmail.com>
2025-04-08 17:45:37 +0300

    Created a new module that holds a trait for Symbol-Index Mapping. This will allow us to define probability models for only a set of symbols (for example only text bytes)

--- src/sim/mod.rs
+++ src/sim/mod.rs
@@ -15,14 +15,19 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#![allow(dead_code)]
+use crate::frequencies::symbol::Symbol;
 
-mod bit_buffer;
-mod frequencies;
-mod models;
-mod number_types;
-mod sim;
+pub trait SymbolIndexMapping {
+    /// Computes a unique index for _symbol_. If _symbol_ is not supported by the mapping, None is
+    /// returned.<br>
+    /// It must be guaranteed that if an index is returned, it lies in the range
+    /// [0, `Self::supported_symbols_count()`).
+    fn get_index(symbol: &Symbol) -> Option<usize>;
 
-fn main() {
-    println!("Hello, world!");
+    /// Returns the symbol to which _index_ is assigned to. If no symbol is mapped to _index_, None
+    /// is returned.
+    fn get_symbol(index: usize) -> Option<Symbol>;
+
+    /// Returns the number of symbols the mapping supports.
+    fn supported_symbols_count() -> usize;
 }

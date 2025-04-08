98157823d849e86876c8195f8141b2f1fe1d84d3
Yair5675 <yairziv5675@gmail.com>
2025-04-08 16:57:47 +0300

    Pre-push formatting

--- src/models/distributions/mod.rs
+++ src/models/distributions/mod.rs
@@ -15,5 +15,5 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pub mod uniform;
 pub mod custom;
+pub mod uniform;
